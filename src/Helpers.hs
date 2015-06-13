e8b9aeb0c9a185bcd528762b420969fad9356e5c
tpajenka <tpajenka@foo.de>
2015-06-13 19:23:47 +0200

    added mapTuple helper functions in Helpers.hs

--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,5 +1,6 @@
-module Helpers (fromLeft, fromRight) where
+module Helpers (fromLeft, fromRight, mapTuple, mapTuple3) where
 
+-- Either functions
 fromLeft :: Either a b -> a
 fromLeft (Left a) = a
 fromLeft _ = error "Helpers.fromLeft: no Left"
@@ -7,3 +8,11 @@ fromLeft _ = error "Helpers.fromLeft: no Left"
 fromRight :: Either a b -> b
 fromRight (Right b) = b
 fromRight _ = error "Helpers.fromLeft: no Left"
+
+
+-- Tuple functions
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple f (x, y) = (f x, f y)
+
+mapTuple3 :: (a -> b) -> (a, a, a) -> (b, b, b)
+mapTuple3 f (x, y, z) = (f x, f y, f z)
