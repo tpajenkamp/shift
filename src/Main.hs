49f0b9008a9dd1ecec668531fbd5030bb0676359
tpajenka <tpajenka@foo.de>
2015-06-24 18:57:40 +0200

    added second window

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 -----------------------------------------------------------------------------
 --
 -- Module      :  ShiftGame.Main
@@ -63,25 +64,39 @@ main = do
    _ <- initGUI
    window <- windowNew
    -- add text view
-   textArea <- textViewNew
-   textViewSetEditable  textArea False
-   textViewSetCursorVisible textArea False
-   monoFnt <- fontDescriptionNew
-   fontDescriptionSetFamily monoFnt "Monospace"
-   widgetModifyFont textArea $ Just monoFnt -- set monospaced font
-   textBuffer <- textViewGetBuffer textArea
-   textBufferSetByteString textBuffer (B.pack "Hallo Welt!")
-   -- widget key focus, key event, link with controller
+   (textArea, ctrl) <- createTextBasedView (initControllerState scenState :: ControllerState IO MatrixScenario)
+   -- widget key focus, key event
    widgetSetCanFocus textArea True
-   let lst = createTextViewLink textBuffer :: UpdateListener IO MatrixScenario
-   ctrl <- initController scenState lst :: IO (ControllerState IO MatrixScenario)
    settingsRef <- newIORef (initSettings scenState, ctrl)
    _ <- textArea `on` keyPressEvent $ keyboardHandler settingsRef 
    -- finalize window
    set window [ containerChild := textArea]
    _ <- window `on` deleteEvent $ lift mainQuit >> return False
    widgetShowAll window
+
+   window2 <- windowNew
+   (textArea2, ctrl2) <- createTextBasedView ctrl
+   widgetSetCanFocus textArea2 True
+   modifyIORef settingsRef (\(s, c) -> (s, ctrl2))
+   _ <- textArea2 `on` keyPressEvent $ keyboardHandler settingsRef 
+   set window2 [ containerChild := textArea2]
+   widgetShowAll window2
    mainGUI
+
+
+createTextBasedView :: ScenarioController ctrl MatrixScenario IO => ctrl -> IO (TextView, ctrl)
+createTextBasedView ctrl = do
+    textArea <- textViewNew
+    textViewSetEditable  textArea False
+    textViewSetCursorVisible textArea False
+    monoFnt <- fontDescriptionNew
+    fontDescriptionSetFamily monoFnt "Monospace"
+    widgetModifyFont textArea $ Just monoFnt -- set monospaced font
+    textBuffer <- textViewGetBuffer textArea
+    -- link with controller
+    let lst = createTextViewLink textBuffer
+    ctrl' <- controllerAddListener ctrl lst
+    return (textArea, ctrl')
 
 initSettings :: Scenario sc => ScenarioState sc -> ControlSettings sc
 initSettings s = ControlSettings { keysLeft  = map (keyFromName . stringToGlib) ["Left", "a", "A"]
