tpajenkamp/shift

commit 4f5b41649934c6127a4f36ddcd71cb475b2b6227

statusbar reacts on win, untested

src/ShiftGame/GTKScenarioView.hs
@@ -271,7 +271,11 @@ instance Scenario sc => UpdateListener (StatusBarListener sc) IO sc where
       _ <- lift $ statusbarPush bar cId (show steps ++ " / " ++ show steps')
       return l
   notifyWin :: (StatusBarListener sc) -> ReaderT (ScenarioState sc) IO (StatusBarListener sc)
-  notifyWin l = return l
+  notifyWin l = do
+      scs <- ask
+      let (steps, steps') = spentSteps scs
+      _ <- lift $ statusbarPush bar cId ("Victory! " ++ show steps ++ " / " ++ show steps')
+      return l
 
 createStatusBarLink :: Scenario sc => Statusbar -> IO (StatusBarListener sc)
 createStatusBarLink bar = do

commit c07872855d99c2a4c8414e81977e5f4f28912adc

fixed compilation bug caused by 4f5b4164

src/ShiftGame/GTKScenarioView.hs
@@ -29,6 +29,7 @@ import           Graphics.UI.Gtk hiding (get, rectangle)
 import           System.Directory (doesFileExist)
 import           System.FilePath (pathSeparator)
 
+
 import ShiftGame.Scenario
 import ShiftGame.ScenarioController
 
@@ -271,7 +272,7 @@ instance Scenario sc => UpdateListener (StatusBarListener sc) IO sc where
       _ <- lift $ statusbarPush bar cId (show steps ++ " / " ++ show steps')
       return l
   notifyWin :: (StatusBarListener sc) -> ReaderT (ScenarioState sc) IO (StatusBarListener sc)
-  notifyWin l = do
+  notifyWin l@(StatusBarListener bar cId) = do
       scs <- ask
       let (steps, steps') = spentSteps scs
       _ <- lift $ statusbarPush bar cId ("Victory! " ++ show steps ++ " / " ++ show steps')